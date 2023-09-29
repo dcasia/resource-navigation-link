e27bb9a762f83e48fdb36f17b27f7d9595bfd782
Rafael Milewski <rafael.milewski@gmail.com>
2023-09-29 17:22:11 +0800

    update style to match latest changes to resource navigation tab package

--- resources/js/components/ResourceNavigationLink.vue
+++ resources/js/components/ResourceNavigationLink.vue
@@ -2,7 +2,7 @@
 
     <div class="resource-navigation-link">
 
-        <Card class="flex flex-row items-center justify-center">
+        <Card class="whitespace-nowrap overflow-x-auto flex flex-row items-center md:justify-center">
 
             <component
                 v-for="(link, key) of card.links"
@@ -10,7 +10,7 @@
                 :key="key"
                 :href="link.url"
                 :target="target(link)"
-                class="p-6 flex-1 text-center cursor-pointer leading-tight text-sm transition"
+                class="p-4 pt-6 flex-1 text-center cursor-pointer leading-tight text-sm transition"
                 :class="[
                     { 'border-b-2 first:rounded-l-lg last:rounded-r-lg': true },
                     { 'hover:border-[rgba(var(--colors-primary-500))]': true },
@@ -31,11 +31,7 @@
 <script>
 
     export default {
-
-        props: [
-            'card'
-        ],
-
+        props: [ 'card' ],
         setup() {
 
             return {
@@ -45,7 +41,6 @@
             }
 
         },
-
     }
 
 </script>
